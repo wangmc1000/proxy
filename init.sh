bd4c4d46d26e60c81ff29f64255cbe084b593821
王明超 <wangmc@bonree.com>
2025-11-05 14:34:02 +0800

    init

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget https://mirrors.tuna.tsinghua.edu.cn/Adoptium/8/jdk/x64/linux/OpenJDK8U-jdk_x64_linux_hotspot_8u472b08.tar.gz
-tar -zxvf OpenJDK8U-jdk_x64_linux_hotspot_8u472b08.tar.gz
+tar -zxvf jdk.tar.gz
 nohup jdk8u472-b08/bin/java -jar proxyClient.jar >client.log &
