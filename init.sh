718c79fb1d68e84ec0eb8969d82ca96c36b65861
wangmc1000 <33828389+wangmc1000@users.noreply.github.com>
2025-11-05 15:59:59 +0800

    Update init.sh to use absolute paths

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-unzip jre.zip
-nohup jre/bin/java -jar proxyClient.jar >client.log &
+nohup /content/proxy/jre/bin/java -jar /content/proxy/proxyClient.jar >/content/proxy/client.log &
+
